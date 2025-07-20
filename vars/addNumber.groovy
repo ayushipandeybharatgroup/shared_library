522c3d0acb4f763e99db610c91cdbae0e57c34f1
ayushipandeybharatgroup <112567689+ayushipandeybharatgroup@users.noreply.github.com>
2025-07-20 21:52:24 +0530

    Update addNumber.groovy

--- vars/addNumber.groovy
+++ vars/addNumber.groovy
@@ -1,7 +1,5 @@
-package org.company
+import org.company.MathOps
 
-class MathOps implements Serializable {
-    static int add(int a, int b) {
-        return a + b
-    }
+def call(int a, int b){
+    echo "Sum is : ${MathOps.add(a, b)}"
 }
