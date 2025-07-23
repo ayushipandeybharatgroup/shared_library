6b46e0378fad729a2ef6d4642eda2c1b628c7c55
ayushipandeybharatgroup <112567689+ayushipandeybharatgroup@users.noreply.github.com>
2025-07-24 01:14:57 +0530

    Update updateIndex.groovy

--- vars/updateIndex.groovy
+++ vars/updateIndex.groovy
@@ -1,8 +1,20 @@
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+
 def call(String name) {
-    // Get current date & time
+    // Set desired time zone (e.g., IST)
+    def timeZone = TimeZone.getTimeZone("Asia/Kolkata")
+    
+    // Get current date & time with time zone
     def now = new Date()
-    def date = now.format("yyyy-MM-dd")
-    def time = now.format("HH:mm:ss")
+
+    def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    def timeFormat = new SimpleDateFormat("HH:mm:ss")
+    dateFormat.setTimeZone(timeZone)
+    timeFormat.setTimeZone(timeZone)
+
+    def date = dateFormat.format(now)
+    def time = timeFormat.format(now)
 
     // Load HTML template from resources
     def template = libraryResource('index.html')
